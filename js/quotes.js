017e611a33137b7398f87db8ef16ede2c9b0a5b1
Darshen Patel <darshen.patel10@gmail.com>
2018-03-28 11:31:29 -0400

    Initial commit

---
+++ js/quotes.js
@@ -0,0 +1,117 @@
+//The quotes.js file contains an array of objects that hold information of 10 quotes we link back to the HTML
+const quotes = [
+
+//Quote Object #1
+  {
+    quote: "In The Joy Of Others, Lies Our Own",
+    source: "Pramukh Swami Maharaj",
+    year: "1971 - 2016"
+  },
+
+//Quote Object #2
+  {
+    quote: "Do The Best, Leave The Rest",
+    source: "Mahant Swami Maharaj",
+    year: "2017 - Present"
+  },
+
+//Quote Object #3
+  {
+    quote: "If you fail, never give up because FAIL means 'First Attempt In Learning'",
+    source: "Dr. A.P.J. Adbul Kalam",
+    year: "2002 - 2015"
+  },
+
+//Quote Object #4
+  {
+    quote: "An Eye For An Eye Makes The Whole World Blind",
+    source: "Mahatma Gandhi",
+    year:"1922 - 1948"
+  },
+
+//Quote Object #5
+  {
+    quote: "The brave man is not he who does not feel afraid, but he who conquers that fear",
+    source: "Nelson Mandela",
+    citation: "Long Walk to Freedom",
+    year: "1994"
+  },
+
+//Quote Object #6
+  {
+    quote: "If you can’t fly then run, if you can’t run then walk, if you can’t walk then crawl, but whatever you do you have to keep moving forward",
+    source: "Dr. Martin Luther King Jr.",
+    citation: "What Is Your Life Blueprint - Barrat Junior High School",
+    year: "October 26, 1967"
+  },
+
+//Quote Object #7
+  {
+    quote: "Government of the people, by the people, for the people, shall not perish from the Earth",
+    source: "President Abraham Lincoln",
+    citation:"Gettysburg Address",
+    year: "November 19, 1863"
+  },
+
+//Quote Object #8
+  {
+    quote: "The only thing we have to fear is fear itself",
+    source: "President Franklin Delano Roosevelt",
+    citation: "FDR's First Inaugural Address",
+    year: "March 3, 1933"
+  },
+
+//Quote Object #9
+  {
+    quote: "Let us therefore brace ourselves to our duties, and so bear ourselves that, if the British Empire and its Commonwealth last for a thousand years, men will say, 'This was their finest hour.'",
+    source: "Winston Churchill",
+    citation: "Finest Hour - House of Commons",
+    year: "June 18, 1940"
+  },
+
+//Quote Object #10
+  {
+    quote: "We choose hope over fear. We see the future not as something out of our control, but as something we can shape for the better through concerted and collective effort.",
+    source: "President Barack Obama",
+    citation: "United Nations General Assembly",
+    year: "September 24, 2014"
+  },
+
+//Quote Object #11
+  {
+    quote: "Imagination is more important than knowledge",
+    source: "Albert Einstein",
+    citation: "The Sunday Evening Post",
+    year: "1929"
+  },
+
+//Quote Object #12
+  {
+    quote: "All our dreams can come true, if we have the courage to pursue them",
+    source: "Walt Disney",
+    },
+
+//Quote Object #13
+  {
+    quote: "Have the courage to follow your heart and intuition. They somehow already know what you truly want to become.",
+    source: "Steve Jobs",
+    citation: "Stanford Commencement Address",
+    year: "2005"
+  },
+
+//Quote Object #14
+  {
+    quote: "Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
+    source: "Bill Gates",
+    citation: "The Road Ahead",
+    year: "1995"
+  },
+
+//Quote Object #15
+  {
+    quote: "If the rules are such that you can’t make progress, then you have to fight the rules.",
+    source: "Elon Musk",
+    citation: "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
+    year: "2015"
+  }
+]
---
+++ js/script.js
@@ -0,0 +1,41 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+function getRandomQuote () {
+  let randomQuote = quotes[Math.floor( Math.random() *  quotes.length )];
+  randomBackgroundColor();
+  return randomQuote;
+}
+
+function printQuote () {
+  let randomQuote = getRandomQuote();
+  html = '<p class="quote">' + randomQuote.quote + '</p>';
+  html += '<p class="source">' + randomQuote.source;
+
+//if citation is available...append onto the HTML script
+  if (randomQuote.citation) {
+    html += '<span class="citation">' + randomQuote.citation + '</span>';
+  }
+
+//if year is available...append onto the HTML script
+  if (randomQuote.year) {
+    html += '<span class="year">' + randomQuote.year + '</span>';
+  }
+
+  let div = document.getElementById('quote-box').innerHTML = html;
+  return html;
+
+}
+
+function randomBackgroundColor () {
+  var r = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var backGroundColor= "rgb(" + r + "," + b + "," + g + ")";
+  document.body.style.background = backGroundColor;
+}
+
+//how to reset time after each click?
+
+setInterval(function(){ printQuote() }, 10000);
