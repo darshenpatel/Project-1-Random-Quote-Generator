42d978743cd331ecd692111a76b7fad57e9425eb
Darshen Patel <darshen.patel10@gmail.com>
2018-03-28 12:42:58 -0400

    Updated Quotes.js
    *Added a categorization property for each object within the quotes array

--- js/quotes.js
+++ js/quotes.js
@@ -5,28 +5,32 @@ const quotes = [
   {
     quote: "In The Joy Of Others, Lies Our Own",
     source: "Pramukh Swami Maharaj",
-    year: "1971 - 2016"
+    year: "1971 - 2016",
+    categorization: "Service"
   },
 
 //Quote Object #2
   {
     quote: "Do The Best, Leave The Rest",
     source: "Mahant Swami Maharaj",
-    year: "2017 - Present"
+    year: "2017 - Present",
+    categorization: "Perseverance"
   },
 
 //Quote Object #3
   {
     quote: "If you fail, never give up because FAIL means 'First Attempt In Learning'",
     source: "Dr. A.P.J. Adbul Kalam",
-    year: "2002 - 2015"
+    year: "2002 - 2015",
+    categorization: "Perseverance"
   },
 
 //Quote Object #4
   {
     quote: "An Eye For An Eye Makes The Whole World Blind",
     source: "Mahatma Gandhi",
-    year:"1922 - 1948"
+    year:"1922 - 1948",
+    categorization: "Forgiveness"
   },
 
 //Quote Object #5
@@ -34,7 +38,8 @@ const quotes = [
     quote: "The brave man is not he who does not feel afraid, but he who conquers that fear",
     source: "Nelson Mandela",
     citation: "Long Walk to Freedom",
-    year: "1994"
+    year: "1994",
+    categorization: "Courage"
   },
 
 //Quote Object #6
@@ -42,7 +47,8 @@ const quotes = [
     quote: "If you can’t fly then run, if you can’t run then walk, if you can’t walk then crawl, but whatever you do you have to keep moving forward",
     source: "Dr. Martin Luther King Jr.",
     citation: "What Is Your Life Blueprint - Barrat Junior High School",
-    year: "October 26, 1967"
+    year: "October 26, 1967",
+    categorization: "Perseverance"
   },
 
 //Quote Object #7
@@ -50,7 +56,8 @@ const quotes = [
     quote: "Government of the people, by the people, for the people, shall not perish from the Earth",
     source: "President Abraham Lincoln",
     citation:"Gettysburg Address",
-    year: "November 19, 1863"
+    year: "November 19, 1863",
+    categorization: "Government"
   },
 
 //Quote Object #8
@@ -58,7 +65,8 @@ const quotes = [
     quote: "The only thing we have to fear is fear itself",
     source: "President Franklin Delano Roosevelt",
     citation: "FDR's First Inaugural Address",
-    year: "March 3, 1933"
+    year: "March 3, 1933",
+    categorization: "Courage"
   },
 
 //Quote Object #9
@@ -66,7 +74,8 @@ const quotes = [
     quote: "Let us therefore brace ourselves to our duties, and so bear ourselves that, if the British Empire and its Commonwealth last for a thousand years, men will say, 'This was their finest hour.'",
     source: "Winston Churchill",
     citation: "Finest Hour - House of Commons",
-    year: "June 18, 1940"
+    year: "June 18, 1940",
+    categorization: "Perseverance"
   },
 
 //Quote Object #10
@@ -74,7 +83,8 @@ const quotes = [
     quote: "We choose hope over fear. We see the future not as something out of our control, but as something we can shape for the better through concerted and collective effort.",
     source: "President Barack Obama",
     citation: "United Nations General Assembly",
-    year: "September 24, 2014"
+    year: "September 24, 2014",
+    categorization: "Bravery"
   },
 
 //Quote Object #11
@@ -82,21 +92,24 @@ const quotes = [
     quote: "Imagination is more important than knowledge",
     source: "Albert Einstein",
     citation: "The Sunday Evening Post",
-    year: "1929"
+    year: "1929",
+    categorization: "Creativity"
   },
 
 //Quote Object #12
   {
     quote: "All our dreams can come true, if we have the courage to pursue them",
     source: "Walt Disney",
-    },
+    categorization: "Courage"
+  },
 
 //Quote Object #13
   {
     quote: "Have the courage to follow your heart and intuition. They somehow already know what you truly want to become.",
     source: "Steve Jobs",
     citation: "Stanford Commencement Address",
-    year: "2005"
+    year: "2005",
+    categorization: "Courage"
   },
 
 //Quote Object #14
@@ -104,7 +117,8 @@ const quotes = [
     quote: "Success is a lousy teacher. It seduces smart people into thinking they can't lose.",
     source: "Bill Gates",
     citation: "The Road Ahead",
-    year: "1995"
+    year: "1995",
+    categorization: "Reflection"
   },
 
 //Quote Object #15
@@ -112,6 +126,7 @@ const quotes = [
     quote: "If the rules are such that you can’t make progress, then you have to fight the rules.",
     source: "Elon Musk",
     citation: "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
-    year: "2015"
+    year: "2015",
+    categorization: "Innovation"
   }
 ]
