3b1bab207b7e85f8b6296528582b9ee259d697b2
Darshen Patel <darshen.patel10@gmail.com>
2018-03-28 13:19:58 -0400

    Updated script.js
    *Added comments

--- js/script.js
+++ js/script.js
@@ -2,36 +2,42 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//The following function pulls a random quote from the array of "quote" objects
 function getRandomQuote () {
   let randomQuote = quotes[Math.floor( Math.random() *  quotes.length )];
+  //When the page returns a random quote, the background color is changed at random too
   randomBackgroundColor();
   return randomQuote;
 }
 
+//The following function pulls the random quote we call and prints it to the page
 function printQuote () {
   let randomQuote = getRandomQuote();
   html = '<p class="quote">' + randomQuote.quote + '</p>';
   html += '<p class="source">' + randomQuote.source;
 
-//if citation is available...append onto the HTML script
+
+//if citation is available...append onto the HTML script and print to the page
   if (randomQuote.citation) {
     html += '<span class="citation">' + randomQuote.citation + '</span>';
   }
 
-//if year is available...append onto the HTML script
+//if year is available...append onto the HTML script and print to the page
   if (randomQuote.year) {
     html += '<span class="year">' + randomQuote.year + '</span>';
   }
 
+//if categorization is available...append onto the HTML script and print ot the page
   if (randomQuote.categorization) {
     html += '<h1>' + randomQuote.categorization + '</h1>';
   }
 
+//the div from the HTML file is called and set equal to the html script we create above
   let div = document.getElementById('quote-box').innerHTML = html;
   return html;
-
 }
 
+//function that randomly changes the background color
 function randomBackgroundColor () {
   var r = Math.floor(Math.random() * 256);
   var b = Math.floor(Math.random() * 256);
